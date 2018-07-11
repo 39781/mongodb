9c10150c1e63cf699d21a492427e868549c7c5f3
Your Name <you@example.com>
2018-07-11 16:38:50 +0530

    fc

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var routes = require('./routes');
-var redirect = require("express-redirect");
-var app = express();
-var session = require('express-session');
-var port = process.env.PORT || 3000;
+var express 	= require('express');
+var bodyParser 	= require('body-parser');
+var mongoose 	= require('mongoose');
+var redirect 	= require("express-redirect");
+var app 		= express();
+var config		= require('./config.js');
+var session 	= require('express-session');
+var port 		= process.env.PORT || 3000;
+
 app.use(session({ secret: 'this-is-a-secret-token',resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }}));
 redirect(app);
 //global.recentInput = "";
@@ -13,9 +15,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
-app.use(routes);
 global.loggedUsers = {};
+
 var server = app.listen(port,function(){
+	
+	mongoose.connect('mongodb://'+config.mongoDB.user+':'+config.mongoDB.pass+'@ds135061.mlab.com:35061/testdb_hari',{ useNewUrlParser: true });
+	var db = mongoose.connection;
+	db.once('open', function() {
+		console.log('mongoose connected');
+		var routes = require('./routes')(mongoose);
+		app.use(routes);		
+	});
+	db.on('error', function(err){
+		console.log(err);
+	});
 	console.log("Application started listening port "+port);		
 });
 
--- routes.js
+++ routes.js
@@ -5,39 +5,38 @@ var request			= require('request');
 var config			= require('./config.js');
 var path			= require("path");	
 var url 			= require('url');	
-var mongoose = require('mongoose');
-console.log(config,'mongodb://'+config.mongoDB.user+':'+config.mongoDB.pass+'@ds135061.mlab.com:35061/testdb_hari');
 
-mongoose.connect('mongodb://'+config.mongoDB.user+':'+config.mongoDB.pass+'@ds135061.mlab.com:35061/testdb_hari',{ useNewUrlParser: true });
-var db = mongoose.connection;
+//console.log(config,'mongodb://'+config.mongoDB.user+':'+config.mongoDB.pass+'@ds135061.mlab.com:35061/testdb_hari');
 
-db.on('error', function(err){
-	console.log(err);
-});
-db.once('open', function() {
-	console.log('mongoDB connecter');
-});
-router.post('/create',function(){
-	
-});
+module.exports = function(mongoose){
 
+	router.post('/createCollection',function(req, res){
+		//var Kitten = mongoose.model('Kitten', kittySchema);
+		res.send('create collection');
+		res.end();
+	})
 
-router.post('/update',function(){
-	
-});
+	router.post('/create',function(){
+		
+	});
 
 
-router.post('/find',function(){
-	
-});
-
-router.post('/delete',function(){
-	
-});
+	router.post('/update',function(){
+		
+	});
 
 
+	router.post('/find',function(){
+		
+	});
+
+	router.post('/delete',function(){
+		
+	});
+
+};
   
-module.exports = router;
+//module.exports = router;
 
 
 
