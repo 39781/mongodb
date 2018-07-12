ddb248a26f4adec151436323d76ce18294b077c9
Your Name <you@example.com>
2018-07-12 11:35:04 +0530

    fc

--- schema.js
+++ schema.js
@@ -1,9 +1,8 @@
 var config = require("./config.js");
 module.exports = function(mongoose){
-	const Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
-	
-	config.schemas.users._id = ObjectId;
+	//const Schema = mongoose.Schema,
+    //ObjectId = Schema.ObjectId;	
+	//config.schemas.users._id = ObjectId;
 		var userSchema 	= 	mongoose.Schema(config.schemas.users);				
 		return {
 			User:mongoose.model('users', userSchema)
